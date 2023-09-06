b5a8c36a468145a66408e5182cc2274b603d2672
khasanov2307 <khasanov2307@icloud.com>
2023-09-07 09:13:56 +1000

    add users_count

--- handlers/client_handlers.py
+++ handlers/client_handlers.py
@@ -39,7 +39,7 @@ async def load_contacts(message: types.Message):
 
 async def show_products(message: types.Message):
     """Хендлер для команды 'продукция' отображает категории продукции"""
-    await message.reply('Актуальный прайс на 01.09.2023\nВыберите пожалуйста категорию⬇️', reply_markup=client_keyboards.kb_category_for_show)
+    await message.reply('Актуальный прайс на 07.09.2023\nВыберите пожалуйста категорию⬇️', reply_markup=client_keyboards.kb_category_for_show)
     await message.delete()
 
 
